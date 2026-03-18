4eae7d0922fe8b0befe92e60703bdffe9530d1b7
fernolo <vaile17738@gmail.com>
2026-03-18 12:30:02 -0400

    Update auth.js

--- API/auth.js
+++ API/auth.js
@@ -1,12 +1,8 @@
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
-  
   const { password } = req.body;
-  const MASTER_PASSWORD = "elpepe"; 
-
-  if (password === MASTER_PASSWORD) {
-    return res.status(200).json({ success: true, token: "adm_sessao_ativa" });
+  if (password === "elpepe") {
+    return res.status(200).json({ success: true });
   }
-  
   res.status(401).json({ success: false });
 }
