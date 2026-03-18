1f90994203982673d5c67cb80854cf107f41d311
fernolo <vaile17738@gmail.com>
2026-03-18 12:48:45 -0400

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
+  const { password } = req.body;
+  if (password === "elpepe") {
+    return res.status(200).json({ success: true });
+  }
+  return res.status(401).json({ success: false });
+}
