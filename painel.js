a864c664fa642c79b5157c220123c9226a59b605
fernolo <vaile17738@gmail.com>
2026-03-18 12:22:44 -0400

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,28 +1,24 @@
 const PainelADM = {
     estaLogado: false,
-    senhaMestra: "123", // Você muda depois
-
-    login: (senha) => {
-        if(senha === PainelADM.senhaMestra) {
+    _key: "jqquuj", 
+    check: (input) => {
+        const coded = input.split('').map(c => String.fromCharCode(c.charCodeAt(0) + 5)).join('');
+        return coded === PainelADM._key;
+    },
+    login: () => {
+        const senha = prompt("Credenciais:");
+        if (PainelADM.check(senha)) {
             PainelADM.estaLogado = true;
-            alert("Modo ADM Ativado!");
-            return true;
+            qs('#adminBtn').textContent = "✅ ADM";
+            renderPosts();
         }
-        alert("Senha Incorreta!");
-        return false;
     },
-
     apagarPost: (index) => {
-        if(!PainelADM.estaLogado) return alert("Acesso negado!");
-        appState.posts.splice(index, 1);
-        save();
-        renderPosts();
-    },
-
-    mudarRegras: (novasRegras) => {
         if(!PainelADM.estaLogado) return;
-        appState.rules = novasRegras;
-        save();
-        alert("Regras atualizadas!");
+        if(confirm("Apagar?")) {
+            appState.posts.splice(index, 1);
+            save();
+            renderPosts();
+        }
     }
 };
