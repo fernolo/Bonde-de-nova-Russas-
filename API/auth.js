15f0087afbe03961136c6794029ed19d2689ca9a
fernolo <vaile17738@gmail.com>
2026-03-18 12:47:40 -0400

    Delete API directory

--- API/auth.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  const { password } = req.body;
-  if (password === "elpepe") {
-    return res.status(200).json({ success: true });
-  }
-  return res.status(401).json({ success: false });
-}
