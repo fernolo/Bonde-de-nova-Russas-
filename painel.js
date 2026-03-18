0e08ebcf1ccda14de4fff697600f9a48e29de153
fernolo <vaile17738@gmail.com>
2026-03-18 12:30:55 -0400

    Update painel.js

--- painel.js
+++ painel.js
@@ -3,19 +3,16 @@ const PainelADM = {
     login: async () => {
         const senha = prompt("Credenciais:");
         if (!senha) return;
-
         try {
             const response = await fetch('/api/auth', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ password: senha })
             });
-
             const data = await response.json();
-
             if (data.success) {
                 PainelADM.estaLogado = true;
-                qs('#adminBtn').textContent = "✅ ADM";
+                if(qs('#adminBtn')) qs('#adminBtn').textContent = "✅ ADM";
                 renderPosts();
             } else {
                 alert("Negado.");
