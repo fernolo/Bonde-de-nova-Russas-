9db5fbdb364f481258f5dde67f4defcf4683ac46
fernolo <vaile17738@gmail.com>
2026-03-18 12:41:02 -0400

    Update painel.js

--- painel.js
+++ painel.js
@@ -4,7 +4,7 @@ const PainelADM = {
         const senha = prompt("Credenciais:");
         if (!senha) return;
         try {
-            const response = await fetch('/api/auth', {
+            const response = await fetch('/API/auth', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ password: senha })
@@ -20,15 +20,15 @@ const PainelADM = {
                     alert("Senha incorreta.");
                 }
             } else {
-                alert("Erro no servidor: Verifique se a pasta /api/auth.js existe na Vercel.");
+                alert("Erro: Verifique se o arquivo auth.js está dentro da pasta API (maiúsculo).");
             }
         } catch (e) {
-            alert("Erro local: A API só funciona quando publicada na Vercel.");
+            alert("Erro de comunicação com o servidor.");
         }
     },
     apagarPost: (index) => {
         if(!PainelADM.estaLogado) return;
-        if(confirm("Apagar?")) {
+        if(confirm("Apagar mensagem?")) {
             appState.posts.splice(index, 1);
             save();
             renderPosts();
