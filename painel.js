90c8c745cc2bf6a17ccabf516b206d6935e998b8
fernolo <vaile17738@gmail.com>
2026-03-18 12:38:45 -0400

    Update painel.js

--- painel.js
+++ painel.js
@@ -9,16 +9,21 @@ const PainelADM = {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ password: senha })
             });
-            const data = await response.json();
-            if (data.success) {
-                PainelADM.estaLogado = true;
-                if(qs('#adminBtn')) qs('#adminBtn').textContent = "✅ ADM";
-                renderPosts();
+            
+            if (response.ok) {
+                const data = await response.json();
+                if (data.success) {
+                    PainelADM.estaLogado = true;
+                    if(qs('#adminBtn')) qs('#adminBtn').textContent = "✅ ADM";
+                    renderPosts();
+                } else {
+                    alert("Senha incorreta.");
+                }
             } else {
-                alert("Negado.");
+                alert("Erro no servidor: Verifique se a pasta /api/auth.js existe na Vercel.");
             }
         } catch (e) {
-            alert("Erro de conexão.");
+            alert("Erro local: A API só funciona quando publicada na Vercel.");
         }
     },
     apagarPost: (index) => {
